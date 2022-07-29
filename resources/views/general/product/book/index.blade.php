a1c17d12aa6afdbfba40777991354cbc1ac1b944
ismail732 <106399158+ismail732@users.noreply.github.com>
2022-07-29 06:21:55 +0500

    product_updatedd

--- resources/views/general/product/book/index.blade.php
+++ resources/views/general/product/book/index.blade.php
@@ -39,7 +39,7 @@
         <div class="card-datatable">
            
 
-          <table class="table table-responsive">
+          <table class="table table-responsive data-table">
             <thead>
               <tr>
                 <th>Product Name</th>
@@ -125,5 +125,28 @@
   <script src="{{ asset(mix('js/scripts/forms/pickers/form-pickers.js')) }}"></script>
   <script src="{{ asset(mix('js/scripts/forms/form-select2.js')) }}"></script>
     {{-- Page js files --}}
-  <script src="{{ asset(mix('js/scripts/tables/table-datatables-advanced.js')) }}"></script>
+    <script>
+      $(document).ready(function() {
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+table = $('.data-table').DataTable({
+    processing: true,
+    serverSide: false,
+    // ajax: "{{ route('subcategory.index') }}",
+    // columns: [
+    //     {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+    //     {data: 'name', name: 'name'},
+    //     {data: 'title', name: 'title'},
+    //     {data: 'description', name: 'description'},
+    //     {data: 'action', name: 'action', orderable: false, searchable: false},          
+    // ]
+});
+});
+    </script>
+ 
 @endsection
